03937895105312599b478f21e4c3c47c2829ad66
Latansa Jaya <latansajaya@gmail.com>
2026-06-01 11:16:08 +0700

    test config db

--- config/db.js
+++ config/db.js
@@ -4,12 +4,13 @@ const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQLDATABASE ||
+    process.env.MYSQL_DATABASE,
   port: parseInt(process.env.MYSQLPORT || "3306")
 });
 
-console.log("DB QUERY TYPE =", typeof db.query);
-
+console.log("SERVER QUERY =", typeof db.query);
+console.log("DATABASE =", process.env.MYSQLDATABASE);
 db.connect((err) => {
   if (err) {
     console.error("MYSQL ERROR:", err);
